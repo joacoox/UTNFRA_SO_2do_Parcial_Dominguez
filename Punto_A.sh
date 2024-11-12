eb7be3e70b972728e1b32ff175d0bb07981814ae
joacoox <joa@Parcial2>
2024-11-12 19:35:04 +0000

    Feature: Ejercico A LVM

---
+++ Punto_A.sh
@@ -0,0 +1,51 @@
+sudo fdisk /dev/sdc
+n
+p
+
+
++5M
+t
+8e
+wq
+
+sudo pvcreate /dev/sdc1
+sudo vgcreate vg_datos /dev/sdc1
+
+sudo fdisk /dev/sdc
+n
+p
+
+
++1.5G
+t
+
+8e
+wq
+
+sudo pvcreate /dev/sdc2
+sudo vgextend vg_datos /dev/sdc2
+
+sudo fdisk /dev/sde
+n
+p
+
+
++512M
+t
+8e
+wq
+
+sudo pvcreate /dev/sde1
+sudo vgcreate vg_temp /dev/sde1
+
+sudo lvcreate -L +5M vg_datos -n lv_docker
+sudo lvcreate -l 100%FREE vg_datos -n lv_workareas
+sudo lvcreate -l 100%FREE vg_temp -n lv_swap
+
+sudo mkfs.ext4 /dev/mapper/vg_datos-lv_docker
+sudo mkfs.ext4 /dev/mapper/vg_datos-lv_workareas
+sudo mkswap /dev/mapper/vg_temp-lv_swap
+
+sudo mount /dev/mapper/vg_datos-lv_docker /var/lib/docker/
+sudo mount /dev/mapper/vg_datos-lv_workareas /work/
+sudo swapon /dev/mapper/vg_temp-lv_swap
